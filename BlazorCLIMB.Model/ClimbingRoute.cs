380e16e41008b4de1687b92c9fe1c29a3d3207ee
IKARDI54 <israel_sanchez_torrero@hotmail.es>
2023-11-09 10:48:18 +0100

    Lista vias

--- BlazorCLIMB.Model/ClimbingRoute.cs
+++ BlazorCLIMB.Model/ClimbingRoute.cs
@@ -9,10 +9,10 @@ namespace BlazorCLIMB.Model
     public class ClimbingRoute
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Grade { get; set; }
-        public string Description { get; set; }
-        public string ClimbingSector { get; set; }
+        public string? Name { get; set; }
+        public string? Grade { get; set; }
+        public string? Description { get; set; }
+        public string? ClimbingSector { get; set; }
         public int ClimbingSchoolId { get; set; }
         public byte[]? Imag { get; set; }
     }
